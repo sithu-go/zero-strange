c79bd4a294f438f067e689eb4cf3e4bca8c9a685
Sithu Strange <sithugolang@gmail.com>
2022-09-22 15:42:41 +0630

    error in usermodel using ExpireCtx

--- service/authentication/model/usermodel.go
+++ service/authentication/model/usermodel.go
@@ -2,13 +2,19 @@ package model
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/core/stores/sqlc"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 var _ UserModel = (*customUserModel)(nil)
+
+var r redis.Redis = redis.Redis{
+	Addr: "127.0.0.1:6379",
+}
 
 type (
 	// UserModel is an interface to be customized, add more methods here,
@@ -16,6 +22,7 @@ type (
 	UserModel interface {
 		userModel
 		FindAll(ctx context.Context) ([]*User, error)
+		FindOneByUsername(ctx context.Context, username string) (*User, error)
 	}
 
 	customUserModel struct {
@@ -37,6 +44,35 @@ func (m *customUserModel) FindAll(ctx context.Context) ([]*User, error) {
 	switch err {
 	case nil:
 		return resp, nil
+	case sqlc.ErrNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+func (m *customUserModel) FindOneByUsername(ctx context.Context, username string) (*User, error) {
+	userUsernameKey := fmt.Sprintf("%s%v", cacheUserUsernamePrefix, username)
+	var resp User
+	err := m.QueryRowIndexCtx(ctx, &resp, userUsernameKey, m.formatPrimary, func(ctx context.Context, conn sqlx.SqlConn, v interface{}) (i interface{}, e error) {
+		query := fmt.Sprintf("select %s from %s where `username` = ? limit 1", userRows, m.table)
+		if err := conn.QueryRowCtx(ctx, &resp, query, username); err != nil {
+			return nil, err
+		}
+		return resp.Id, nil
+	}, m.queryPrimary)
+	switch err {
+	case nil:
+		cuname := fmt.Sprintf("cache:user:username:%v", "strange")
+		err := r.ExpireCtx(ctx, cuname, 10)
+		if err != nil {
+			return &resp, err
+		}
+		err = r.ExpireCtx(ctx, fmt.Sprintf("cache:user:id:%v", 1), 10)
+		if err != nil {
+			return &resp, err
+		}
+		return &resp, nil
 	case sqlc.ErrNotFound:
 		return nil, ErrNotFound
 	default:
